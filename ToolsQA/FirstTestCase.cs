c94596f4cfe0a2dfd2159302bdf190eb40d75557
Hanno Vahtrik <hannovahtrik@hotmail.com>
2019-09-15 22:55:04 +0300

    Commit first

---
+++ ToolsQA/FirstTestCase.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace ToolsQA
+{
+    class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://transport.tallinn.ee/#tallinna-linn/map";
+        }
+    }
+}
